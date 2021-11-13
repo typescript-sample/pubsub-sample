cd7aed9844ae06d956cda45b25775063952c0b20
Duc Nguyen <minhduc140583@gmail.com>
2021-11-13 15:32:44 +0700

    Format code

--- src/route.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from 'express';
-import { ApplicationContext } from './context';
-
-export function route(app: Application, ctx: ApplicationContext): void {
-  const health = ctx.healthController;
-  app.get('/health', health.check);
-}
